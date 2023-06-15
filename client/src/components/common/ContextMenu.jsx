719d5c603b5075b6c12cd95435b5fd33dcdccbbb
NqDyK2 <nguyenquyduyabc@gmail.com>
2023-06-15 16:46:50 +0700

    Feat: Update click change Avatar in onboarding 1:37:03

--- client/src/components/common/ContextMenu.jsx
+++ client/src/components/common/ContextMenu.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 
-function ContextMenu() {
-  return <div>ContextMenu</div>;
+function ContextMenu({ options, cordinates, context, setContextMenu }) {
+  const contextMenuRef = useRef(null);
+  const handleClick = (e, callback) => {};
+  return (
+    <div
+      className={` bg-dropdown-background fixed py-2 z-[100] shadow-xl`}
+      ref={contextMenuRef}
+      style={{
+        top: cordinates.y,
+        left: cordinates.x,
+      }}
+    >
+      <ul>
+        {options.map(({ name, callback }) => (
+          <li key={name} onClick={() => handleClick(e, callback)}>
+            <span className="text-white">{name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ContextMenu;
