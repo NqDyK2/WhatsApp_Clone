8902f6b4c853560c9eb8e8d849619c11e0ef6c17
NqDyK2 <nguyenquyduyabc@gmail.com>
2023-06-15 11:36:23 +0700

    Feat:features login with google add axios to FE write API in BE

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,19 +1,27 @@
+import { CHECK_USER_ROUTE } from "@/utils/ApiRoutes";
 import { firebaseAuth } from "@/utils/FirebaseConfig";
+import axios from "axios";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import React from "react";
 import { FcGoogle } from "react-icons/fc"
 import { toast } from "react-toastify";
 
 function login() {
+  const router = useRouter()
   const handleLogin = async () => {
     const provider = new GoogleAuthProvider()
-    const { user: { displayName:name, email, photoURL: profileImage } } = await signInWithPopup(firebaseAuth, provider);
-    try{
-      if(email) {
-        
+    const { user: { displayName: name, email, photoURL: profileImage } } = await signInWithPopup(firebaseAuth, provider);
+    try {
+      if (email) {
+        const { data } = await axios.post(CHECK_USER_ROUTE, { email });
+        console.log({ data });
+        if (!data.status) {
+          router.push("/onboarding")
+        }
       }
-    } catch(err) {
+    } catch (err) {
       console.log(err);
     }
   };
