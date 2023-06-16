86a6559593b29c1484ae23f26fe7d2344af0d14b
NqDyK2 <nguyenquyduyabc@gmail.com>
2023-06-16 15:57:20 +0700

    Feat: Done onboardpage but should test take photo with camera

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -6,52 +6,73 @@ import axios from "axios";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React from "react";
-import { FcGoogle } from "react-icons/fc"
+import React, { useEffect } from "react";
+import { FcGoogle } from "react-icons/fc";
 import { toast } from "react-toastify";
 
 function login() {
-  const router = useRouter()
+  const router = useRouter();
 
-  const [{}, dispatch] = useStateProvider();
+  const [{ userInfo, newUser }, dispatch] = useStateProvider();
+
+  useEffect(() => {
+    if (userInfo?.id && !newUser) router.push("/");
+  }, [userInfo, newUser]);
 
   const handleLogin = async () => {
-    const provider = new GoogleAuthProvider()
-    const { user: { displayName: name, email, photoURL: profileImage } } = await signInWithPopup(firebaseAuth, provider);
+    const provider = new GoogleAuthProvider(); 
+    const {
+      user: { displayName: name, email, photoURL: profileImage },
+    } = await signInWithPopup(firebaseAuth, provider);
     try {
       if (email) {
         const { data } = await axios.post(CHECK_USER_ROUTE, { email });
-        dispatch({ type: reducerCases.SET_NEW_USER, newUser: true })
         if (!data.status) {
-          dispatch({type: reducerCases.SET_USER_INFO,userInfo: {
-            name, 
-            email, 
-            profileImage, 
-            status: "",
-          }})
-          router.push("/onboarding")
+          dispatch({ type: reducerCases.SET_NEW_USER, newUser: true });
+          dispatch({
+            type: reducerCases.SET_USER_INFO,
+            userInfo: {
+              name,
+              email,
+              profileImage,
+              status: "",
+            },
+          });
+          router.push("/onboarding");
         }
+      } else {
+        const { id, name, email, profilePicture: profileImage, status } = data;
+        dispatch({
+          type: reducerCases.SET_USER_INFO,
+          userInfo: {
+            id,
+            name,
+            email,
+            profileImage,
+            status,
+          },
+        });
+        router.push("/");
       }
     } catch (err) {
       console.log(err);
     }
   };
-  return <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-6">
-    <div className="flex items-center justify-center gap-2 text-white">
-      <Image
-        src="/whatsapp.gif" alt="WhatsApp" height={300} width={300}
-      />
-      <span className="text-7xl ">
-        WhatsApp
-      </span>
+  return (
+    <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-6">
+      <div className="flex items-center justify-center gap-2 text-white">
+        <Image src="/whatsapp.gif" alt="WhatsApp" height={300} width={300} />
+        <span className="text-7xl ">WhatsApp</span>
+      </div>
+      <button
+        className="flex items-center justify-center gap-7 bg-search-input-container-background p-5 rounded-lg"
+        onClick={handleLogin}
+      >
+        <FcGoogle className="text-4xl" />
+        <span className="text-white text-2xl">Login With Google</span>
+      </button>
     </div>
-    <button className="flex items-center justify-center gap-7 bg-search-input-container-background p-5 rounded-lg" onClick={handleLogin}>
-      <FcGoogle className="text-4xl" />
-      <span className="text-white text-2xl">
-        Login With Google
-      </span>
-    </button>
-  </div>;
+  );
 }
 
 export default login;
