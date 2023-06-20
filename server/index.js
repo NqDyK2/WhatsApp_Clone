c879ce8cc0633f4552867518ffffa50ad1c2652d
NqDyK2 <nguyenquyduyabc@gmail.com>
2023-06-20 17:40:13 +0700

    Fix: Update image show to done send image message

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use("/api/auth", AuthRoutes)
 app.use("/api/messages", MessageRoutes)
 
-app.use("uploads/images", express.static("uploads/images"));
+app.use("/uploads/images", express.static("uploads/images"));
 
 
 const server = app.listen(process.env.PORT, () => {
