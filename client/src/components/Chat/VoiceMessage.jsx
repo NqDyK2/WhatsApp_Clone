4ce327af17cb6c5c3e62e4dba3f4bd85d44a6f92
NqDyK2 <nguyenquyduyabc@gmail.com>
2023-06-30 17:41:54 +0700

    update 6:57:39

--- client/src/components/Chat/VoiceMessage.jsx
+++ client/src/components/Chat/VoiceMessage.jsx
@@ -1,7 +1,84 @@
-import React from "react";
+import { useStateProvider } from "@/context/StateContext";
+import React, { useEffect, useRef, useState } from "react";
 
-function VoiceMessage() {
-  return <div>VoiceMessage</div>;
+function VoiceMessage({ message }) {
+  const [{ currentChatUser, userInfo }] = useStateProvider();
+  const [audioMessage, setAudioMessage] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentPlaybackTime, setCurrentPlaybackTime] = useState(0);
+  const [totalDuration, setTotalDuration] = useState(0);
+
+  const waveFormRef = useRef(null);
+  const waveform = useRef(null);
+
+  useEffect(() => {
+    if (waveform.current === null) {
+      waveform.current = WaveSurfer.create({
+        container: waveFormRef.current,
+        waveColor: "#ccc",
+        progressColor: "#4a9eff",
+        cursorColor: "#7ae3c3",
+        barWidth: 2,
+        height: 30,
+        responsive: true,
+      });
+      waveform.current.on("finish", () => {
+        setIsPlaying(false);
+      });
+    }
+
+    return () => {
+      waveform.current.destroy();
+    };
+  }, []);
+
+  useEffect(() => {
+    const audioURL = `${HOST}/${message.message}`;
+    const audio = new Audio(audioURL);
+    setAudioMessage(audio);
+    waveform.current.load(audioURL);
+    waveform.current.on("ready", () => {
+      setTotalDuration(waveform.current.getDuration);
+    });
+  }, [message.message]);
+
+  const handlePlayRecording = () => {
+    if (recordedAudio) {
+      waveform.stop();
+      waveform.play();
+      recordedAudio.play();
+      setisPlaying(true);
+    }
+  };
+
+  const handlePauseRecording = () => {
+    waveform.stop();
+    recordedAudio.pause();
+    setIsPlaying(false);
+  };
+
+  useEffect(() => {
+    if (recordedAudio) {
+      const updatePlaybackTime = () => {
+        setCurrentPlaybackTime(recordedAudio.currentTime);
+      };
+      recordedAudio.addEventListener("timeupdate", updatePlaybackTime);
+      return () => {
+        recordedAudio.removeEventListener("timeupdate", updatePlaybackTime);
+      };
+    }
+  }, [recordedAudio]);
+
+  const formatTime = (time) => {
+    if (isNaN(time)) return "00:00";
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  };
+
+  return <div></div>;
 }
 
 export default VoiceMessage;
