5e2b95600cddc4ab7aac7eaac1df07621bd84c82
NqDyK2 <nguyenquyduyabc@gmail.com>
2023-07-04 11:33:00 +0700

    Feat: Sidebar contacts

--- client/src/components/Chatlist/List.jsx
+++ client/src/components/Chatlist/List.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { useStateProvider } from "@/context/StateContext";
+import { reducerCases } from "@/context/constants";
+import { GET_INITIAL_CONTACTS_ROUTE } from "@/utils/ApiRoutes";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import ChatListItem from "./ChatListItem";
 
 function List() {
-  return <div className="bg-search-input-container-background flex-auto overflow-auto max-h-full custom-scrollbar"></div>;
+  const [{ userInfo, userContacts }, dispatch] = useStateProvider();
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const {
+          data: { users, onlineUsers },
+        } = await axios.get(`${GET_INITIAL_CONTACTS_ROUTE}/${userInfo.id}`);
+        dispatch({ type: reducerCases.SET_ONLINE_USERS, onlineUsers });
+        dispatch({ type: reducerCases.SET_USER_CONTACTS, userContacts: users });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (userInfo?.id) getContacts(); 
+    getContacts();
+  }, [userInfo]);
+
+  return (
+    <div className="bg-search-input-container-background flex-auto overflow-auto max-h-full custom-scrollbar">
+      {userContacts.map((contact) => (
+        <ChatListItem data={contact} key={contact.id} />
+      ))}
+    </div>
+  );
 }
 
 export default List;
