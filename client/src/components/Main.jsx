336d0b17a0e3efbaf660452acdb399c750faa346
NqDyK2 <nguyenquyduyabc@gmail.com>
2023-07-04 16:37:35 +0700

    Feat: Update voice and Video Call UI

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -11,11 +11,23 @@ import { reducerCases } from "@/context/constants";
 import Chat from "./Chat/Chat";
 import { Socket, io } from "socket.io-client";
 import SearchMessages from "./Chat/SearchMessages";
+import VideoCall from "./Call/VideoCall";
+import VoiceCall from "./Call/VoiceCall";
 
 function Main() {
   const [redirectLogin, setRedirectLogin] = useState(false);
-  const [{ userInfo, currentChatUser, messagesSearch }, dispatch] =
-    useStateProvider();
+  const [
+    {
+      userInfo,
+      currentChatUser,
+      messagesSearch,
+      videoCall,
+      voiceCall,
+      incomingVoiceCall,
+      incomingVideoCall,
+    },
+    dispatch,
+  ] = useStateProvider();
   const [socketEvent, setSocketEvent] = useState(false);
   const socket = useRef();
 
@@ -91,21 +103,32 @@ function Main() {
 
   return (
     <>
+      {videoCall && (
+        <div className="h-screen w-screen max-h-full overflow-hidden ">
+          <VideoCall />
+        </div>
+      )}
+      {voiceCall && (
+        <div className="h-screen w-screen max-h-full overflow-hidden ">
+          <VoiceCall />
+        </div>
+      )}
+      {
+        !videoCall && !voiceCall && 
       <div className="grid grid-cols-main h-screen w-screen max-h-screen max-w-full overflow-hidden">
         <ChatList />
         {currentChatUser ? (
           <div className={messagesSearch ? "grid grid-cols-2" : "grid-cols-2"}>
             <Chat />
-            {
-              messagesSearch && <SearchMessages />
-            }
+            {messagesSearch && <SearchMessages />}
           </div>
         ) : (
           <Empty />
-        )}
-        {/* <Empty /> */}
+          )}
         <Chat />
+        {/* <Empty /> */}
       </div>
+        }
     </>
   );
 }
