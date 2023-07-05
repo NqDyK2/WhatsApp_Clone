cb04b9c334c10f410336156f6b44827d59a6b2a3
NqDyK2 <nguyenquyduyabc@gmail.com>
2023-07-05 14:29:04 +0700

    Feat: Update Call with API zego cloud but still bug should to test again

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_ZEGO_APP_ID: 1872474823,
+    NEXT_PUBLIC_ZEGO_SERVER_ID:  "d9605f790f568dba83b7c493ae7df370",
+  },
   images: {
     domains: ["localhost"]
   }
